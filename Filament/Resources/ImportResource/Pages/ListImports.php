65787c64f10c896d4ae8deb424c55cf281ca261a
marco76tv <marco.sottana@gmail.com>
2024-11-29 16:22:23 +0100

    up

--- Filament/Resources/ImportResource/Pages/ListImports.php
+++ Filament/Resources/ImportResource/Pages/ListImports.php
@@ -4,19 +4,14 @@ declare(strict_types=1);
 
 namespace Modules\Job\Filament\Resources\ImportResource\Pages;
 
-use Filament\Tables;
 use Filament\Actions;
-use Filament\Tables\Table;
-use Modules\UI\Enums\TableLayoutEnum;
+use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
-use Filament\Resources\Pages\ListRecords;
-use Modules\Xot\Filament\Traits\TransTrait;
 use Modules\Job\Filament\Resources\ImportResource;
 use Modules\Xot\Filament\Pages\XotBaseListRecords;
 
 class ListImports extends XotBaseListRecords
 {
-    
     protected static string $resource = ImportResource::class;
 
     public function getTableColumns(): array
@@ -56,15 +51,6 @@ class ListImports extends XotBaseListRecords
             Tables\Actions\DeleteBulkAction::make(),
             // ]),
         ];
-    }
-
-    public function table(Table $table): Table
-    {
-        return $table
-            ->columns($this->getTableColumns())
-            ->filters($this->getTableFilters())
-            ->actions($this->getTableActions())
-            ->bulkActions($this->getTableBulkActions());
     }
 
     protected function getHeaderActions(): array
