a7cd69f5c3a2b346d87a3d4bb63bfbea627c771e
marco76tv <marco.sottana@gmail.com>
2025-01-01 22:28:07 +0100

    up

---
+++ resources/lang/it/job_batch.php
@@ -0,0 +1,16 @@
+<?php return array (
+  'actions' => 
+  array (
+    'prune_batches' => 
+    array (
+      'label' => 'prune_batches',
+    ),
+  ),
+  'fields' => 
+  array (
+    'failed_job_ids' => 
+    array (
+      'label' => 'failed_job_ids',
+    ),
+  ),
+);
