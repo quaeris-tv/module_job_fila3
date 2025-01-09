628e1ef5db3a96411cfa6f2f7cabc84dae94cd3d
marco76tv <marco.sottana@gmail.com>
2025-01-09 09:02:32 +0100

    up

--- lang/it/failed_import_row.php
+++ lang/it/failed_import_row.php
@@ -1,46 +1,63 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    // 'resources' => 'Risorse',
-    'pages' => 'Pagine',
-    'widgets' => 'Widgets',
-    'navigation' => [
-        'name' => 'Failed Import Row',
-        'plural' => 'Failed Import Rows',
-        'group' => [
-            'name' => 'Import/Export',
-        ],
-    ],
-    'fields' => [
-        'id' => ['label' => 'ID'],
-        'data' => ['label' => 'Data'],
-        'import_id' => ['label' => 'Import Id'],
-        'validation_error' => ['label' => 'Validation Error'],
-        'name' => 'Nome',
-        'guard_name' => 'Guard',
-        'permissions' => 'Permessi',
-        'updated_at' => 'Aggiornato il',
-        'first_name' => 'Nome',
-        'last_name' => 'Cognome',
-        'select_all' => [
-            'name' => 'Seleziona Tutti',
-            'message' => '',
-        ],
-    ],
-    'actions' => [
-        'import' => [
-            'fields' => [
-                'import_file' => 'Seleziona un file XLS o CSV da caricare',
-            ],
-        ],
-        'export' => [
-            'filename_prefix' => 'Aree al',
-            'columns' => [
-                'name' => 'Nome area',
-                'parent_name' => 'Nome area livello superiore',
-            ],
-        ],
-    ],
-];
+<?php return array (
+  'pages' => 'Pagine',
+  'widgets' => 'Widgets',
+  'navigation' => 
+  array (
+    'name' => 'Failed Import Row',
+    'plural' => 'Failed Import Rows',
+    'group' => 
+    array (
+      'name' => 'Import/Export',
+    ),
+    'label' => 'failed import row.navigation',
+  ),
+  'fields' => 
+  array (
+    'id' => 
+    array (
+      'label' => 'ID',
+    ),
+    'data' => 
+    array (
+      'label' => 'Data',
+    ),
+    'import_id' => 
+    array (
+      'label' => 'Import Id',
+    ),
+    'validation_error' => 
+    array (
+      'label' => 'Validation Error',
+    ),
+    'name' => 'Nome',
+    'guard_name' => 'Guard',
+    'permissions' => 'Permessi',
+    'updated_at' => 'Aggiornato il',
+    'first_name' => 'Nome',
+    'last_name' => 'Cognome',
+    'select_all' => 
+    array (
+      'name' => 'Seleziona Tutti',
+      'message' => '',
+    ),
+  ),
+  'actions' => 
+  array (
+    'import' => 
+    array (
+      'fields' => 
+      array (
+        'import_file' => 'Seleziona un file XLS o CSV da caricare',
+      ),
+    ),
+    'export' => 
+    array (
+      'filename_prefix' => 'Aree al',
+      'columns' => 
+      array (
+        'name' => 'Nome area',
+        'parent_name' => 'Nome area livello superiore',
+      ),
+    ),
+  ),
+);
