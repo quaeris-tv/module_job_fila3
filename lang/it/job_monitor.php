1a74600564c63c1f38ddc61d143a0736778556c8
marco76tv <marco.sottana@gmail.com>
2025-01-20 22:36:30 +0100

    up

--- lang/it/job_monitor.php
+++ lang/it/job_monitor.php
@@ -1,21 +1,23 @@
-<?php
-
-return [
-    'pages' => 'Pagine',
-    'widgets' => 'Widgets',
-    'navigation' => [
-        'name' => 'Job Monitor',
-        'plural' => 'Job Monitor',
-        'group' => [
-            'name' => 'Jobs',
-        ],
-        'label' => 'job monitor',
-    ],
-    'fields' => [
-        'name' => 'Nome',
-        'guard_name' => 'Guard',
-        'permissions' => 'Permessi',
-        'updated_at' => 'Aggiornato il',
-        'first_name' => 'Nome',
-    ],
-];
+<?php return array (
+  'pages' => 'Pagine',
+  'widgets' => 'Widgets',
+  'navigation' => 
+  array (
+    'name' => 'Job Monitor',
+    'plural' => 'Job Monitor',
+    'group' => 
+    array (
+      'name' => 'Jobs',
+    ),
+    'label' => 'job monitor',
+    'sort' => 82,
+  ),
+  'fields' => 
+  array (
+    'name' => 'Nome',
+    'guard_name' => 'Guard',
+    'permissions' => 'Permessi',
+    'updated_at' => 'Aggiornato il',
+    'first_name' => 'Nome',
+  ),
+);
