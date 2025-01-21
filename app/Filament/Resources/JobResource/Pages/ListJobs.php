35895272eef14b75fe39d225c30851ddb2605634
marco76tv <marco.sottana@gmail.com>
2025-01-21 12:17:41 +0100

    up

--- app/Filament/Resources/JobResource/Pages/ListJobs.php
+++ app/Filament/Resources/JobResource/Pages/ListJobs.php
@@ -8,37 +8,44 @@ use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\ViewColumn;
 use Modules\Job\Filament\Resources\JobResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
+use Filament\Tables\Columns\Column;
 
 class ListJobs extends XotBaseListRecords
 {
     protected static string $resource = JobResource::class;
 
+    /**
+     * @return array<string, \Filament\Tables\Columns\Column>
+     */
     public function getListTableColumns(): array
     {
         return [
-            TextColumn::make('id')
+            'id' => TextColumn::make('id')
                 ->searchable()
                 ->sortable(),
-            TextColumn::make('queue')
+            'queue' => TextColumn::make('queue')
                 ->searchable()
                 ->sortable(),
-            TextColumn::make('attempts')
+            'payload' => TextColumn::make('payload')
+                ->wrap()
+                ->searchable(),
+            'attempts' => TextColumn::make('attempts')
                 ->numeric()
                 ->sortable(),
-            TextColumn::make('status')
+            'status' => TextColumn::make('status')
                 ->badge()
                 ->color(fn (string $state): string => match ($state) {
                     'running' => 'primary',
                     'waiting' => 'warning',
                     default => 'danger',
                 }),
-            TextColumn::make('reserved_at')
+            'reserved_at' => TextColumn::make('reserved_at')
                 ->dateTime()
                 ->sortable(),
-            TextColumn::make('available_at')
+            'available_at' => TextColumn::make('available_at')
                 ->dateTime()
                 ->sortable(),
-            TextColumn::make('created_at')
+            'created_at' => TextColumn::make('created_at')
                 ->dateTime()
                 ->sortable(),
             ViewColumn::make('payload')
