1a92e4ae3c7024a67b5b73d3ea79c14fa4b186bb
marco76tv <marco.sottana@gmail.com>
2025-02-04 12:57:25 +0100

    up

--- lang/it/job_status.php
+++ lang/it/job_status.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 return [
@@ -41,54 +40,3 @@ return [
         ],
     ],
 ];
-=======
-<?php return array (
-  'resources' => 'Risorse',
-  'pages' => 'Pagine',
-  'widgets' => 'Widgets',
-  'navigation' => 
-  array (
-    'name' => 'job status',
-    'plural' => 'job status',
-    'group' => 
-    array (
-      'name' => 'Jobs',
-    ),
-    'label' => 'job status',
-    'sort' => 36,
-  ),
-  'fields' => 
-  array (
-    'name' => 'Nome',
-    'guard_name' => 'Guard',
-    'permissions' => 'Permessi',
-    'updated_at' => 'Aggiornato il',
-    'first_name' => 'Nome',
-    'last_name' => 'Cognome',
-    'select_all' => 
-    array (
-      'name' => 'Seleziona Tutti',
-      'message' => '',
-    ),
-  ),
-  'actions' => 
-  array (
-    'import' => 
-    array (
-      'fields' => 
-      array (
-        'import_file' => 'Seleziona un file XLS o CSV da caricare',
-      ),
-    ),
-    'export' => 
-    array (
-      'filename_prefix' => 'Aree al',
-      'columns' => 
-      array (
-        'name' => 'Nome area',
-        'parent_name' => 'Nome area livello superiore',
-      ),
-    ),
-  ),
-);
->>>>>>> origin/dev
